4e7bd48fffdd32b953630b7185ca902ff10f032c
NinguemBravo <58083466+NinguemBravo@users.noreply.github.com>
2020-06-13 17:30:13 -0300

    oi

---
+++ New Unity Project/Assets/Scripts/Player/Armas/Trocar_Equipamento.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Trocar_Equipamento : MonoBehaviour
+{
+    [Range(0.0f, 5.0f)]  public int Equipnum;
+    public GameObject[] Equipamentos;
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+    private void Trocar() {
+        
+    }
+
+}
